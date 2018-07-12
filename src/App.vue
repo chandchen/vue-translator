3c589f3867aedf2d972aea1c528714b617a55fb0
Chand Chen <changduchen@gmail.com>
2018-07-12 19:48:59 +0800

    Refactor demo to support multi page switch

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,68 @@
 <template>
   <div id="app">
-    <h1>&#12288</h1>
-    <h1>Translate Online</h1>
-    <h5 class="text-muted">Get a quick, free translation!</h5>
-    <p></p>
-    <translateForm v-on:formSubmitEvent="translateText"></translateForm>
-    <hr class="my-4">
-    <TranslateOutput v-text="result"></TranslateOutput>
+
+    <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+      <div class="container">
+        <a class="navbar-brand" href="https://github.com/chandchen/">Hit&Run</a>
+        <!-- <router-link class="navbar-brand" to="/">Hit&Run</router-link> -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item" :class="{active:show==1}" @click="aShow">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item" :class="{active:show==2}" @click="bShow">
+              <router-link class="nav-link" to="/translate">Translate</router-link>
+            </li>
+            <li class="nav-item" :class="{active:show==3}" @click="cShow">
+              <router-link class="nav-link" to="/dictionary">Dictionary</router-link>
+            </li>
+            <li class="nav-item" :class="{active:show==4}" @click="dShow">
+              <router-link class="nav-link" to="/about">About</router-link>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+    <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 
-import TranslateForm from './components/TranslateForm'
-import TranslateOutput from './components/TranslateOutput'
 
 export default {
   name: 'App',
   data: function() {
     return {
-      result: ""
+      result: "",
+      show: 1,
     }
   },
   components: {
-    TranslateForm,
-    TranslateOutput
+
   },
   methods: {
-    translateText: function(data, lang) {
-      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180711T035615Z.672207da8c81f299.bf6af01d3901ceec57da26b1c5af7fd737cc5d04&lang='+lang+'&text='+data).then((response) => {
-        this.result = response.body.text[0]
-      })
+    aShow: function() {
+      this.show = 1;
+    },
+    bShow: function() {
+      this.show = 2;
+    },
+    cShow: function() {
+      this.show = 3;
+    },
+    dShow: function() {
+      this.show = 4;
     }
   }
 }
